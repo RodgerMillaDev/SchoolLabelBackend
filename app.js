68bea1115a209d89bc113887ddc1980b952a64ef
Rodger Milla Dev <rodgermilladev@gmail.com>
2026-03-07 16:02:55 +0300

    New Update

--- app.js
+++ app.js
@@ -141,7 +141,7 @@ app.post("/paystack-webhook", express.json({ type: "*/*" }), async (req, res) =>
 });
 
 app.post("/trxnStatus", async (req, res) => {
-  const { refCode, userId } = req.body;
+  const { refCode, userId,destination,phone } = req.body;
 
   const options = {
     hostname: "api.paystack.co",
@@ -170,17 +170,45 @@ app.post("/trxnStatus", async (req, res) => {
         const email = paystackRespData.data.customer.email;
 
         if (status === "success") {
-          await firestore.collection("transactions").doc(refCode).set({
-            reference: refCode,
+
+          // 1️⃣ Get user document
+          const userRef = firestore.collection("users").doc(userId);
+          const userSnap = await userRef.get();
+
+          if (!userSnap.exists) {
+            return res.status(404).json({ error: "User not found" });
+          }
+
+          const userData = userSnap.data();
+          const cartItems = userData.cartItems || [];
+          const userName = userData.userName || "";
+
+          // 2️⃣ Save order
+          await firestore.collection("Orders").doc(refCode).set({
+            transactionId: refCode,
+            userId,
+            destination,
+            phone,
+            userName,
             email,
             amount,
+            cartItems,
             status,
-            userId,
-            verifiedAt: serverTimestamp(),
+            createdAt: serverTimestamp(),
+          });
+
+          await firestore.collection("stats").doc("earnings").update({
+  totalRevenue: admin.firestore.FieldValue.increment(totalCost)
+});
+
+          // 3️⃣ Clear cart
+          await userRef.update({
+            cartItems: []
           });
         }
 
         res.json(paystackRespData);
+
       } catch (error) {
         console.log(error);
         res.status(500).json({ error: "Verification failed" });
@@ -195,3 +223,6 @@ app.post("/trxnStatus", async (req, res) => {
 
   request.end();
 });
+
+
+    
