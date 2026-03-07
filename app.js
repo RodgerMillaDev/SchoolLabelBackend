6ab6d2cd11d66aac0e50416ec463234ecf3e7de9
Rodger Milla Dev <rodgermilladev@gmail.com>
2026-03-07 17:30:09 +0300

    New Update

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cors({ origin: "*" }));
 
 const port = process.env.PORT;
-const adminUIDS = [process.env.adminOne];
+const adminUIDS = [process.env.adminOne,process.env.adminTwo];
 adminUIDS.forEach((uid) => {
   admin
     .auth()
