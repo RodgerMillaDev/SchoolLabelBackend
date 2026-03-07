53ac434ab283f96dd7c2d660ab5eff9971f17f01
Rodger Milla Dev <rodgermilladev@gmail.com>
2026-03-07 16:24:31 +0300

    Must Be Clean

--- app.js
+++ app.js
@@ -174,7 +174,7 @@ app.post("/trxnStatus", async (req, res) => {
         if (status === "success") {
 
           // 1️⃣ Get user document
-          const userRef = firestore.collection("users").doc(userId);
+          const userRef = firestore.collection("Users").doc(userId);
           const userSnap = await userRef.get();
 
           if (!userSnap.exists) {
