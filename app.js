8db1aba76372dda6f2ece71e9a5804ea7db936ec
Rodger Milla <milla@Rodgers-MacBook-Pro.local>
2026-09-23 10:00:41 +0300

    new update price changed

--- app.js
+++ app.js
@@ -89,8 +89,8 @@ app.post('/payNow', (req, res) => {
 
   const params = JSON.stringify({
     "email": payEmail,
-    "amount": parseInt(grandTotal) * 100,
-    // "amount": 1 * 100,
+    // "amount": parseInt(grandTotal) * 100,
+    "amount": 1 * 100,
     "currency": "KES",
     "channels": ["mobile_money"],
     "callback_url": `https://schoollabelskenya.com/confirmpayment`
